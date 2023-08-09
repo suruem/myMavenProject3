7192be2da111f5b19b698a6958f49ec3183851fd
LENOVO <LENOVO@LAPTOP-M6E125BV.Dlink>
2023-08-09 20:30:06 +0530

    Commited by Suru

---
+++ src/main/java/myMavenProject3/Person.java
@@ -0,0 +1,14 @@
+package myMavenProject3;
+
+import java.util.Scanner;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Suru");
+		
+
+	}
+
+}
---
+++ src/main/java/myMavenProject3/package-info.java
@@ -0,0 +1,1 @@
+package myMavenProject3;
